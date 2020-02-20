7b61d5638728cbaa5e907ba648a50f1161a49abe
vincentzercher <vincent.zercher@laposte.net>
2020-02-20 10:19:08 +0100

    Commit pour pull

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -190,48 +190,6 @@ class CommonController extends AbstractController
     }
 
     /**
-     * @Route("/excursions/create", name="app_create_excursions")
-     *
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     */
-    public function createExcursion(Request $request)
-    {
-        $excursion = new Excursion();
-        $excursion->setParticipantLimit(10);
-
-        $user = $this->getUser();
-
-
-        /** @noinspection PhpParamsInspection */
-        $excursion->setOrganizer($user);
-
-
-        $form = $this->createForm(ExcursionPostType::class, $excursion);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-//            $excursion = $form->getData();
-            $site = $form->get('site')->getData();
-            $city = $form->get('place')->get('city')->getData();
-            $place = $form->get('place')->getData();
-            $excursion->setSite($site);
-            $excursion->setState(0);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($city);
-            $entityManager->persist($place);
-            $entityManager->persist($excursion);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_excursions');
-        }
-
-        return $this->render('excursions/create.html.twig', [
-            'createExcursionForm' => $form->createView()
-        ]);
-    }
-
-    /**
      * @Route("/excursions/details/{id}", name="app_details_excursions")
      */
     public function detailsExcursion(EntityManagerInterface $em, $id): Response
